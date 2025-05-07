d88be625d3da9ae5374a323c9ee28b7b99a660d7
kuritaku <shoty893@yahoo.co.jp>
2025-05-07 15:04:48 +0900

    バックエンドの課題完了

---
+++ todo_app_front/main.js
@@ -0,0 +1,37 @@
+const API_URL = 'http://127.0.0.1:5000/tasks';
+
+async function renderTasks() {
+    const res = await fetch(API_URL);
+    const tasks = await res.json();
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = '';
+
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>${task.title}</span>
+            <button onclick="deleteTask(${task.id})">削除</button>
+        `;
+        taskList.appendChild(li);
+    });
+}
+
+document.getElementById('add-task').addEventListener('click', async () => {
+    const title = document.getElementById('new-task').value.trim();
+    if (!title) return;
+
+    await fetch(API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({title}),
+    });
+    document.getElementById('new-task').value = '';
+    renderTasks();
+})
+
+async function deleteTask(id) {
+    await fetch(`${API_URL}/${id}`, { method: 'DELETE' });
+    renderTasks();
+}
+
+renderTasks();
